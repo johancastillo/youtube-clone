5bb69299272ca37294583c667b731b0076642f66
Johan Castillo <jcjohan2707@gmail.com>
2021-07-12 14:06:48 -0400

    Communication components

--- src/app/components/video-card/video-card.component.ts
+++ src/app/components/video-card/video-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-video-card',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoCardComponent implements OnInit {
 
-  
+  @Input() data: any;
+
 
   constructor() { }
 
